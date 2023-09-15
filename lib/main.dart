2402098a7b0b04e0da2c266cecb165affae8f530
Shubham Kislay <shubhamkislay@Shubhams-MacBook-Pro.local>
2023-09-15 21:24:10 +0530

    Added Firebase setup for android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  debugPrint('didinitializeApp');
   runApp(const MyApp());
 }
 
@@ -51,14 +56,19 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    FirebaseAuth.instance.signInAnonymously().then((value) {
+
+      setState(() {
+        // This call to setState tells the Flutter framework that something has
+        // changed in this State, which causes it to rerun the build method below
+        // so that the display can reflect the updated values. If we changed
+        // _counter without calling setState(), then the build method would not be
+        // called again, and so nothing would appear to happen.
+        print("User ID is ${value.user?.uid}");
+        _counter++;
+      });
     });
+
   }
 
   @override
